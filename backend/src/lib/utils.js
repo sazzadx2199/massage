e0424efce93db8aaf187fd9c54cb48d1f1b88409
Sazzad Islam <124683129+SAZZAD-404@users.noreply.github.com>
2025-11-08 14:27:39 +0600

    Fix cookie sameSite for cross-origin requests

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -14,8 +14,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
-    secure: ENV.NODE_ENV === "development" ? false : true,
+    sameSite: ENV.NODE_ENV === "production" ? "none" : "strict", // Allow cross-site cookies in production
+    secure: ENV.NODE_ENV === "production" ? true : false, // Secure must be true when sameSite is none
   });
 
   return token;
